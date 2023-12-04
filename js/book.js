ee5897a7ac00c293c236940151bbf5badc4db9ac
ankhgerelenkhtur <ankhgerelenkhtur@mail.ru>
2023-12-04 18:32:25 +0800

    a

---
+++ js/book.js
@@ -0,0 +1,36 @@
+
+    let total = 0;
+    let count = 0;
+
+    function addToBookingCard(button) {
+        const movieContainer = button.closest('.movie');
+        const title = movieContainer.dataset.title;
+        const genre = movieContainer.dataset.genre;
+        const duration = movieContainer.dataset.duration;
+        const price = parseFloat(movieContainer.dataset.price);
+
+        total += price;
+        count++;
+
+        const shoppingIcon = document.querySelector('.shopping-icon');
+        shoppingIcon.setAttribute('data-count', count);
+
+        const bookingList = document.getElementById('bookingList');
+        const listItem = document.createElement('li');
+        listItem.textContent = `${title} - ${genre} (${duration}) - $${price}`;
+        bookingList.appendChild(listItem);
+
+        const totalPriceElement = document.getElementById('totalPrice');
+        totalPriceElement.textContent = `Нийт үнэ: MNT ${total.toFixed(0)}`;
+    }
+
+    function showBookingCard() {
+        const bookingCard = document.getElementById('bookingCard');
+        bookingCard.style.display = 'block';
+    }
+
+    function hideBookingCard() {
+        const bookingCard = document.getElementById('bookingCard');
+        bookingCard.style.display = 'none';
+    }
+
--- js/change.js
+++ js/change.js
@@ -1,53 +1,34 @@
-var currentBackgroundIndex = 0;
-var backgrounds = ["image/background1.jpg", "image/napback.jpeg", "image/pacific.jpeg", "image/cod.jpeg"];
-var intervalId;
+let total = 0;
+let count = 0;
 
-function changeBackground(direction) {
-    if (direction === 'prev') {
-        currentBackgroundIndex = (currentBackgroundIndex - 1 + backgrounds.length) % backgrounds.length;
-    } else if (direction === 'next') {
-        currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
-    }
+function addToBookingCard(button) {
+    const movieContainer = button.closest('.movie');
+    const title = movieContainer.dataset.title;
+    const genre = movieContainer.dataset.genre;
+    const duration = movieContainer.dataset.duration;
+    const price = parseFloat(movieContainer.dataset.price);
 
-    document.getElementById('backgroundImage').style.opacity = 0; 
-    setTimeout(function () {
-        document.getElementById('backgroundImage').src = backgrounds[currentBackgroundIndex];
-        document.getElementById('backgroundImage').style.opacity = 2; 
-        updatePosterContent();
-    }, 800); 
+    total += price;
+    count++;
+
+    const shoppingIcon = document.querySelector('.shopping-icon');
+    shoppingIcon.setAttribute('data-count', count);
+
+    const bookingList = document.getElementById('bookingList');
+    const listItem = document.createElement('li');
+    listItem.textContent = `${title} - ${genre} (${duration}) - $${price}`;
+    bookingList.appendChild(listItem);
+
+    const totalPriceElement = document.getElementById('totalPrice');
+    totalPriceElement.textContent = `Нийт үнэ: ${total.toFixed(0)}`;
 }
 
-function updatePosterContent() {
-    
-    var movieDetails = [
-        { title: 'OPPENHEIMER', date: '2023.10.15', genre: 'DRAMA/HISTORY', background: 'url(/image/poster.jpeg) center/cover no-repeat', boxShadow: '0 4px 32px 0 #F00' }, 
-        { title: 'NAPOLEON', date: '2023.11.30', genre: 'DRAMA/HISTORY', background: 'url(/image/post2.jpg) center/cover no-repeat', boxShadow: '0 4px 32px 0 rgb(192, 57, 66)' },
-        { title: 'PACIFIC RIM', date: '2023.10.15', genre: 'ACTION/FICTION', background: 'url(/image/rim.jpg) center/cover no-repeat', boxShadow: '0 4px 32px 0 rgb(0, 153, 255)' },
-        { title: '13 HOURS', date: '2023.10.15', genre: 'ACTION/FICTION', background: 'url(/image/post4.jpeg) center/cover no-repeat', boxShadow: '0 4px 32px 0 rgb(0, 0, 0)' }
-    
-    ];
+function showBookingCard() {
+    const bookingCard = document.getElementById('bookingCard');
+    bookingCard.style.display = 'block';
+}
 
-    var posterSection = document.querySelector('.poster1');
-
-    var currentMovie = movieDetails[currentBackgroundIndex];
-    var backgroundElement = document.getElementById('backgroundImage');
-    backgroundElement.style.transition = 'opacity 0.5s ease-in';
-
-
-    posterSection.setAttribute('style', `background: ${currentMovie.background}; box-shadow: ${currentMovie.boxShadow}`);
-
-    posterSection.innerHTML = `
-        <article>
-            <a href="delgerengui.html">
-                <h2 class="mname">${currentMovie.title}</h2>
-            </a>
-            <div class="maininfo">
-                <img src="/image/calendar.png" alt="Calendar icon" width="15" height="15" />
-                <p class="time">${currentMovie.date}</p>
-                <p class="genre">ТӨРӨЛ: ${currentMovie.genre}</p>
-            </div>
-            <button class="secondary" aria-label="Book now">Захиалах</button>
-            <a class="more" href="delgerengui.html">Дэлгэрэнгүй</a>
-        </article>
-    `;
+function hideBookingCard() {
+    const bookingCard = document.getElementById('bookingCard');
+    bookingCard.style.display = 'none';
 }
